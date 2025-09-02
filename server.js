3b240be0772a53c149e9194f2c7b920ed2934d33
tahahhany11192 <tahah8833@gmail.com>
2025-09-02 21:22:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const ChatRoom = require('./models/ChatRoom');
 const searchRoutes = require("./routes/searchRoutes");
 
 // Configuration
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 const FRONTEND_ORIGIN = process.env.CORS_ORIGIN || 'https://tahahhany11192.github.io/CS-EslamHatem-Frontend';
 const MONGO_URI = process.env.MONGO_URI || '*';
 
